50107a446c4c95421f5f6489953dbe7bebf85353
x711Li <x711Li@gmail.com>
2012-06-16 14:05:41 -0400

    Initial commit for xfer

---
+++ xfer/landing.php
@@ -0,0 +1,47 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<title>AboutYou</title>
+
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Oxygen" >
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css" >
+</head>
+<body style="background:#EDEDED;">
+<div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <button type="button" class="btn btn-navbar" data-toggle="collapse" data-target=".nav-collapse">
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="brand" href="./index.html" style="text-decoration:none;"><h5 style="padding:0; line-height:20px; margin:0;">AboutYou</h5></a>
+          <div class="nav-collapse collapse">
+          </div>
+        </div>
+      </div>
+    </div>
+<div style="margin:auto; width:1000px;">
+<div id="profile" class="padfront" style="background:url('images/home/hero.png'); width:600px; height:741px; display:inline-block; vertical-align:top; margin-right:25px;">
+</div>
+<div id="panel" style="display:inline-block; vertical-align:top; width:350px; text-align:center;">
+<h4 style="color:#66C;">WELCOME TO <span style="color:#000;">ABOUTYOU</span></h4>
+Have you ever wanted a formal website to show employers?<br />
+<br />
+So when people google your name, they see a professional profile and not your social page?
+
+<h5>THAT'S WHERE WE COME IN</h5>
+In the click of a button, we will turn your social profile into a professional representation.<br />
+<br />
+Don't worry, we filter out those nasty status updates, slightly hung over pictures and gramatically incorrect descriptions.<br />
+<br />
+We just display the good stuff. So you can represent yourself on the web in an elegant, professional manner.<br />
+<br />
+<h5>CLICK BELOW TO GET STARTED</h5>
+<br />
+<img src="images/home/log_in_fb_button.png" />
+</div>
+</div>
+</body>
+</html>
---
+++ xfer/profile.php
@@ -0,0 +1,35 @@
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+
+<title>AboutYou</title>
+
+<link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Oxygen" >
+<link rel="stylesheet" type="text/css" href="css/bootstrap.css" >
+</head>
+<body style="background:#EDEDED;">
+<div class="navbar navbar-fixed-top">
+      <div class="navbar-inner">
+        <div class="container">
+          <a class="brand" href="./index.html" style="text-decoration:none;"><h5 style="padding:0; line-height:20px; margin:0;">AboutYou</h5></a>
+          <div class="nav-collapse collapse">
+          </div>
+        </div>
+      </div>
+    </div>
+<div style="text-align:center; margin:auto; width:1000px; height:800px; background:url('images/home/linen_pattern.png')" class="padfront">
+    <div style="width:700px; height:800px; display:inline-block; vertical-align:top;">
+        <div class="padfront" style="height:150px; width:650px; margin: 40px auto;">
+        </div>
+    </div>
+    <div style="width:250px; height:800px; display:inline-block; vertical-align:top; float:right;">
+        <div style="width:200px; height:200px;"></div>
+        <h5 style="margin:0px;">Khallil Mangalji</h5>
+        <h6 style="margin:0px;">Born May 4th | Agile Engineer</h6>
+        <h5 style="margin-bottom:0px;">Recent Activity</h5>
+        <div class="padfront" style="height:400px; width:200px; margin:auto;">
+        </div>
+    </div>
+</div>
+</body>
+</html>
