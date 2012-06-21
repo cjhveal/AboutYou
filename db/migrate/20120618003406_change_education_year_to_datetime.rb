186878d8acdfbb6150270538acd0361c4a4d8bbb
Cody Veal <cveal05@gmail.com>
2012-06-21 00:58:23 -0400

    fixing datetime migration

--- db/migrate/20120618003406_change_education_year_to_datetime.rb
+++ db/migrate/20120618003406_change_education_year_to_datetime.rb
@@ -2,12 +2,12 @@ class ChangeEducationYearToDatetime < ActiveRecord::Migration
   def up
     add_column :educations, :end_date, :datetime
     Education.all.each {|e| e.update_attribute :end_date, DateTime.parse("1-5-#{e.year}") if e.year}
-    remove_column :educations, :year, :datetime
+    remove_column :educations, :year
   end
 
   def down
     add_column :educations, :year, :string
     Education.all.each {|e| e.update_attribute :year, e.end_date.year if e.end_date}
-    remove_column :educations, :year, :string
+    remove_column :educations, :end_date
   end
 end
